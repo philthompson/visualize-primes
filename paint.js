8022a1f4e01b51138ff9d10a5506ac20ce184293
Phil <phil@philthompson.me>
2021-10-15 14:04:26 -0700

    Read/write settings to/from URL search parameters

--- paint.js
+++ paint.js
@@ -1,10 +1,10 @@
-var points = [];
+const points = [];
 var totalLength = 0;
 // 'R'ight, 'L'eft, 'U'p, 'D'own
 var direction = 'R';
 
-var dcanvas = document.getElementById('dc');
-var dContext = dcanvas.getContext('2d');
+const dCanvas = document.getElementById('dc');
+const dContext = dCanvas.getContext('2d');
 
 function isPrime(n) {
   if (n < 1) {
@@ -50,34 +50,66 @@ function computeNextPoint(n, x, y) {
 }
 
 var start = function() {
-  var x = 0.0;
-  var y = 0.0;
+  // for whatever reason, using the URL hash parameters doesn't cause
+  //   the page to reload and re-draw the visualization, so we will
+  //   use the URL search parameters instead (and use history.pushState
+  //   when actually drawing the thing to ensure the URL is kept
+  //   up-to-date with what is being drawn without reloading the page)
+  var urlParams = new URLSearchParams(document.location.search);
+
+  // default settings that work on my monitor
+  var params = {
+    "v": 1,
+    "n": 60000,
+    "lineWidth": 1.0,
+    "scale": 1.5,
+    "offsetX": 0.3,
+    "offsetY": 0.37
+  };
+
+  // on my monitor, good test settings for 60,000 points
+  //params = {"v": 1, "n": 60000, "lineWidth": 1.0, "scale": 1.5, "offsetX": 0.3, "offsetY": 0.37};
+  // on my monitor, good test settings for 500 points
+  //params = {"v": 1, "n": 500, "lineWidth": 3.0, "scale": 20.0, "offsetX": 0.3, "offsetY": -0.2};
+
+  // only change default settings if a known version of settings is given
+  if (urlParams.has('v') && urlParams.get('v') == 1) {
+    if (urlParams.has('n')) {
+      params.n = 1.0 * parseInt(urlParams.get('n'));
+    }
+    if (urlParams.has('lineWidth')) {
+      params.lineWidth = parseFloat(urlParams.get('lineWidth'));
+    }
+    if (urlParams.has('scale')) {
+      params.scale = parseFloat(urlParams.get('scale'));
+    }
+    if (urlParams.has('offsetX')) {
+      params.offsetX = parseFloat(urlParams.get('offsetX'));
+    }
+    if (urlParams.has('offsetY')) {
+      params.offsetY = parseFloat(urlParams.get('offsetY'));
+    }
+  }
+  console.log(params);
+
+  var nextPoint = getPoint(0.0, 0.0);
   direction = "U"; // start with 'U'p
 
-  // 60,000 seems like a good non-overlapping point
-  //     - this fits on my monitor: drawPoints(1.0, 1.5, canvas.width * 0.8, canvas.height * 0.87);
-  //     - this is a good scale on my monitor: drawPoints(1.0, 3.0, canvas.width * 0.8, canvas.height * 0.87);
-  for (var i = 1.0; i < 60000.0; i+=1.0) {
+  for (var i = 1.0; i < params.n; i+=1.0) {
     if (isPrime(i)) {
       //console.log(i + " is prime");
-      // only add points right before we change direction
-      points.push(getPoint(x, y));
+      // only add points right before we change direction, and once at the end
+      points.push(nextPoint);
       changeDirection();
     }
     // find the next point according to direction and current location
-    var nextPoint = computeNextPoint(i, x, y)
-    x = nextPoint.x;
-    y = nextPoint.y;
+    nextPoint = computeNextPoint(i, nextPoint.x, nextPoint.y);
     //points.push(nextPoint);
   }
 
   setDScaleVars(dContext);
-  var canvas = dContext.canvas;
-  // works for 60,000 points
-  //drawPoints(1.0, 1.5, canvas.width * 0.8, canvas.height * 0.87);
-  drawPoints(1.0, 1.52, canvas.width * 0.8, canvas.height * 0.87);
-  // works for 500 points
-  //drawPoints(3.0, 20.0, canvas.width * 0.8, canvas.height * 0.2);
+
+  drawPoints(params);
 };
 
 var pixelColor = function(imgData, x, y) {
@@ -103,7 +135,14 @@ function setDScaleVars(dCtx) {
 }
 
 
-function drawPoints(lineWidth, scale, offsetX, offsetY) {
+function drawPoints(params) {
+  history.pushState("", document.title, document.location.pathname + "?" + new URLSearchParams(params).toString());
+  const canvas = dContext.canvas;
+  const lineWidth = params.lineWidth;
+  const scale = params.scale;
+  const offsetX = canvas.width * (0.5 + params.offsetX);
+  const offsetY = canvas.height * (0.5 + params.offsetY);
+
   fillBlack(dContext);
   console.log("drawing [" + points.length + "] points with a total length of [" + totalLength + "]");
 
--- readme.md
+++ readme.md
@@ -1,9 +1,10 @@
 ### Visualize Primes
 
-Javascript tool to draw an image using a sequence of integers.
+Javascript tool that draws an image using a sequence of integers.
 
 A continuous line is drawn.  The line continues straight 1 unit for each non-prime integer.  When an integer is prime, the line turns, then continues 1 unit.
 
-### Licence
+### License
 
-MIT  
+MIT
+

34ca890b589741413415c07608a93f2a862ccb5b
Phil <phil@philthompson.me>
2021-10-29 19:17:01 -0700

    Remove extraneous negative signs

--- paint.js
+++ paint.js
@@ -247,12 +247,12 @@ const sequences = [{
 },{
   "name": "Primes-X-Y-neg-mod-3",
   "desc": "Where each plotted point <code>(x,y)</code> consists of the primes, in order.  " +
-          "Those points are (2,-3), (5,7), (11,13), and so on.<br/><br/>" +
+          "Those points are (2,3), (5,7), (11,13), and so on.<br/><br/>" +
           "Then we take the sum of the digits of both the <code>x</code> and <code>y</code> of each point.<br/>" +
           "If that sum, mod 3, is 1, the <code>x</code> is negated.<br/>" +
           "If that sum, mod 3, is 2, the <code>y</code> is negated.<br/><br/>" +
           "After applying the negation rule, the first three plotted points become:<br/>" +
-          "<code>(2,-3) → sum digits = 5&nbsp;&nbsp;mod 3 = 2 → -y → (2,-3)</code><br/>" +
+          "<code>(2,3)&nbsp;&nbsp;→ sum digits = 5&nbsp;&nbsp;mod 3 = 2 → -y → (2,-3)</code><br/>" +
           "<code>(5,7)&nbsp;&nbsp;→ sum digits = 12 mod 3 = 0 →&nbsp;&nbsp;&nbsp;&nbsp;→ (5,7)</code><br/>" +
           "<code>(11,13)→ sum digits = 6&nbsp;&nbsp;mod 3 = 0 →&nbsp;&nbsp;&nbsp;&nbsp;→ (11,13)</code>",
   "computePointsAndLength": function(privContext) {
